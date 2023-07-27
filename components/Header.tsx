ac6bbf840b0ec0c756bb51905732c43ecec30525
Senix9 <jdpb95@gmail.com>
2023-07-27 03:38:37 -0500

    1 hour

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,11 @@
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft } from "react-icons/rx"
+import { RxCaretRight } from "react-icons/rx"
+import { HiHome } from "react-icons/hi"
+import { BiSearch } from "react-icons/bi"
+import Button from "./Button";
+
 
 
 interface HeaderProps {
@@ -41,14 +46,57 @@ const Header: React.FC<HeaderProps> = ({
             gap-x-2
             items-center
             ">
-                <button className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
+                <button onClick={() => router.back()} className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
                     <RxCaretLeft className="text-white" size={35}/>
+                </button>
+                <button onClick={() => router.forward()} className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
+                    <RxCaretRight className="text-white" size={35}/>
                 </button>
 
             </div>
 
+            <div className="flex md:hidden gap-x-2 items-center">
+                <button className="rounded-full p-2 bg-white flex items-center hover:opacity-75 transition">
+                    <HiHome className="text-black" size={20}>
+
+                    </HiHome>
+
+                </button>
+                <button className="rounded-full p-2 bg-white flex items-center hover:opacity-75 transition">
+                    <BiSearch className="text-black" size={20}>
+
+                    </BiSearch>
+
+                </button>
+
+            </div>
+
+            <div className="flex justify-between items-center gap-x-4">
+                <>
+                <div>
+                    
+
+                    <Button className="
+                    bg-transparent
+                    text-neutral-300
+                    font-medium">
+                        Sign up
+                    </Button>
+                </div>
+                <div>
+                <Button className="
+                    bg-white
+                    px-6
+                    py-2">
+                        Log in
+                    </Button>
+                </div>
+                </>
+
+            </div>
+
         </div>
-            
+           {children} 
         </div>
     );
 }
